0fb6d4825e09ae213a114d1e0868565083388b07
bikrambk24 <bk.bikram16@gmail.com>
2022-11-15 19:18:21 +0545

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello React
-        </a>
-      </header>
+   <>
+   <h2>Register here</h2>
+   <form>
+
+    <div className = "firstName">
+      <label htmlFor="firstName">First Name </label>
+      <input type="text" name='firstName' id='firstName' placeholder="First name"  />
     </div>
+
+    <div className = "lastName">
+      <label htmlFor="lastName">Last Name </label>
+      <input type="text" name='lastName' id='lastName' placeholder="Last name" />
+    </div>
+
+    <div className = "address">
+      <label htmlFor="address">Address </label>
+      <input type="text" name='address' id='address' placeholder="Address" />
+    </div>
+
+    <div className = "gender">
+      <label htmlFor="gender">Gender </label>
+                          <select>
+                            <option>Male</option>
+                            <option>Female</option>
+                            <option>LGBTQ</option>  
+                          </select>
+    </div>
+
+    <div className = "password">
+      <label htmlFor="password">Password </label>
+      <input type="password" name='password' id='password' placeholder="Password" />
+    </div>
+
+    <div className = "dob">
+      <label htmlFor="dob">Date of Birth </label>
+      <input type="date" name='dob' id='dob' />
+    </div>
+
+    <button> Submit</button>
+
+   </form>
+   
+   </>
   );
 }
 
